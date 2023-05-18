af20e623674d299efe4dc445ec4f4f1d2fe817cb
WillarDraco <a1863235@adelaide.edu.au>
2023-05-18 13:22:13 +0930

    Edited Barbarian.h

---
+++ Barbarian.h
@@ -0,0 +1,23 @@
+#ifndef BARBARIAN_H
+#define BARBARIAN_H
+#include "Player.h"
+#include "Weapon.h"
+#include "Monster.h"
+
+class Barbarian : protected Player {
+    private:
+        int strength; // represents strength of a player - affects melee damage
+        Weapon** barbarian_weapon; // currently equipped weapon
+    public:
+        Barbarian(); // sets all data members to 0 or “ ”
+        Barbarian(int level); // sets strength and health based on level, exp = 0, current weapon = “”;
+
+        void equipWeapon(Weapon* barbarian_weapon); // sets current weapon (weapon bonuses)
+        void unequipWeapon(Weapon* barbarian_weapon); // sets current weapon to  “” (removes weapon bonuses)
+        void castSpell(); // wizard only attack that uses mana and does damage based on mana and equipped weapon
+
+        void swingWeapon(); // barbarian only attack that does damage based on strength and equipped weapon
+
+        void rest(); // heals player for turn - based on total health
+};
+#endif
