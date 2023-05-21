34d87fad277a6b1db14b84714c71c829d709548a
Archie111111 <archie.rowe@student.adelaide.edu.au>
2023-05-21 14:28:19 +0930

    upgrade files

---
+++ Upgrade.h
@@ -0,0 +1,18 @@
+#ifndef UPGRADE_H
+#define UPGRADE_H
+
+class Upgrade{
+    protected:
+    bool upgrade_strength;
+    bool upgrade_agility;
+    bool upgrade_mana;
+
+
+    public:
+    Upgrade(); //default constructer
+    
+    
+
+
+};
+#endif
