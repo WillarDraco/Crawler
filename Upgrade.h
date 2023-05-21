f73eb09c1b6af4c0ca4f3108e664d10bd7d13d13
Archie111111 <archie.rowe@student.adelaide.edu.au>
2023-05-21 14:52:45 +0930

    more upgrade

--- Upgrade.h
+++ Upgrade.h
@@ -1,6 +1,8 @@
 #ifndef UPGRADE_H
 #define UPGRADE_H
 
+#include "Player.h"
+
 class Upgrade{
     protected:
     bool upgrade_strength;
@@ -11,6 +13,10 @@ class Upgrade{
     public:
     Upgrade(); //default constructer
     
+    void choose_strength(); //sets upgrade strength to true
+    void choose_agility(); //sets upgrade agility to true
+    void choose_mana(); //sets upgrade agility to true
+
     
 
 
