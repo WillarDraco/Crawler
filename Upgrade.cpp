51c3393834e6a23c07a78eb5f61b8bfa7a5d1cd3
Archie111111 <archie.rowe@student.adelaide.edu.au>
2023-05-21 16:00:01 +0930

    upgrades

--- Upgrade.cpp
+++ Upgrade.cpp
@@ -2,9 +2,6 @@
 #include "Barbarian.h"
 #include "Archer.h"
 #include "Wizard.h"
-#include "Barbarian.h"
-#include "Archer.h"
-#include "Wizard.h"
 #include "Upgrade.h"
 
 void Upgrade::upgrade_strength(Barbarian* barbarian){
@@ -14,58 +11,16 @@ void Upgrade::upgrade_strength(Barbarian* barbarian){
     barbarian->set_strength(new_strength);
 };
 
-void upgrade_agility(Archer* archer){; 
+
+void Upgrade::upgrade_agility(Archer* archer){; 
 
     int new_agility = archer->get_agility() + 2;
     
     archer->set_agility(new_agility);
 };
 
-void upgrade_mana(Wizard* wizard){
 
-    int new_mana = wizard->get_max_mana() + 2;
-    
-    wizard->set_max_mana(new_mana);
-};#include "Player.h"
-#include "Upgrade.h"
-
-void Upgrade::upgrade_strength(Barbarian* barbarian){
-
-    int new_strength = barbarian->get_strength() + 2;
-
-    barbarian->set_strength(new_strength);
-};
-
-void upgrade_agility(Archer* archer){; 
-
-    int new_agility = archer->get_agility() + 2;
-    
-    archer->set_agility(new_agility);
-};
-
-void upgrade_mana(Wizard* wizard){
-
-    int new_mana = wizard->get_max_mana() + 2;
-    
-    wizard->set_max_mana(new_mana);
-};#include "Player.h"
-#include "Upgrade.h"
-
-void Upgrade::upgrade_strength(Barbarian* barbarian){
-
-    int new_strength = barbarian->get_strength() + 2;
-
-    barbarian->set_strength(new_strength);
-};
-
-void upgrade_agility(Archer* archer){; 
-
-    int new_agility = archer->get_agility() + 2;
-    
-    archer->set_agility(new_agility);
-};
-
-void upgrade_mana(Wizard* wizard){
+void Upgrade::upgrade_mana(Wizard* wizard){
 
     int new_mana = wizard->get_max_mana() + 2;
     
