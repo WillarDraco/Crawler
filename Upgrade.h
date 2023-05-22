aa3831f792b91b9728ccc977413d64e5e7ff2023
Archie111111 <archie.rowe@student.adelaide.edu.au>
2023-05-22 15:20:39 +0930

    uprades

--- Upgrade.h
+++ Upgrade.h
@@ -18,10 +18,7 @@ class Upgrade{
     void upgrade_mana(Wizard* wizard); //function for upgrading mana
 
     void upgrade_weapon_bonus (Weapon* weapon); //function for upgrading the weapon bonus
-    void switch_weapon(); //function for changing weapon
 
     
-
-
 };
 #endif
