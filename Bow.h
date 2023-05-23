5b65673d7dc0889f366ac4f4c160192a3ea9d925
willardraco <a1863235@adelaide.edu.au>
2023-05-23 23:26:55 +0930

    Made bow.h and bow.cpp working for new damage calcs

--- Bow.h
+++ Bow.h
@@ -6,6 +6,6 @@
 class Bow : public Weapon {
     public:
         Bow(); // Default constructor
-        Bow(std::string name, int stat_bonus, int level); // Constructor for a bow
+        Bow(std::string name, int level); // Constructor for a bow
 };
 #endif
