efda4d71d1810d249caee621a83b44289e192099
Archie111111 <archie.rowe@student.adelaide.edu.au>
2023-05-26 10:43:57 +0930

    weapon stats

--- Crossbow.cpp
+++ Crossbow.cpp
@@ -10,8 +10,8 @@ Crossbow::Crossbow(std::string name, int level) { // Constructor for a bow
 
     weapon_stats[0] = level * 5; // represents the max health increase
     weapon_stats[1] = level * 5; // current health increase
-    weapon_stats[4] = level * 4; // Agility increase
-    weapon_stats[5] = level * (1/8); // Dodge chance increase
+    weapon_stats[4] = level * 4; // class stat increase
+    weapon_stats[5] = level * (1/16); // Dodge chance increase
     weapon_stats[6] = level * 3; // Defense stat increase
     weapon_stats[7] = level * 2; // Crit chance increase
     weapon_stats[8] = level * 5; // Max ammo increase
